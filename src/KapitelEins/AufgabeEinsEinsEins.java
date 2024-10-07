dab89f5d34a3d57bbdae116059299a49cd2880bc
AbdullahMuradSari <abdullah.m.sari@gmail.com>
2024-10-07 15:37:01 +0200

    1.1.1

---
+++ src/KapitelEins/AufgabeEinsEinsEins.java
@@ -0,0 +1,10 @@
+package KapitelEins;
+
+public class AufgabeEinsEinsEins {
+    /**
+     * Diese Methode gibt "Aye Captain!" aus
+     */
+    public static void captainAusgabe() {
+        System.out.println("Aye Captain!");
+    }
+}
---
+++ src/KapitelEins/KapitelEinsMain.java
@@ -0,0 +1,8 @@
+package KapitelEins;
+import KapitelEins.AufgabeEinsEinsEins;
+
+public class KapitelEinsMain {
+    public static void main(String[] args) {
+        AufgabeEinsEinsEins.captainAusgabe();
+    }
+}
