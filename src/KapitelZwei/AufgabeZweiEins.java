f6388a3cf35aca92e6bb1316fd7432d5a32f9e17
AbdullahMuradSari <abdullah.m.sari@gmail.com>
2024-10-10 19:31:59 +0200

    2.1

---
+++ src/KapitelZwei/AufgabeZweiEins.java
@@ -0,0 +1,10 @@
+package KapitelZwei;
+
+public class AufgabeZweiEins {
+    /**
+     * Diese Methode gibt einen bestimmten Text mit einer bestimmten Formatierung aus
+     */
+    public static void circleMainMethod(){
+        System.out.printf( "<svg height=\"400\" width=\"1000\">\n  <circle cx=\"100\" cy=\"100\" r=\"50\" />\n</svg>" );
+    }
+}
---
+++ src/KapitelZwei/KapitelZweiMain.java
@@ -0,0 +1,10 @@
+package KapitelZwei;
+
+public class KapitelZweiMain {
+    public static void main(String[] args) {
+        /**
+         * Aufgabe 2.1
+         */
+        AufgabeZweiEins.circleMainMethod();
+    }
+}
